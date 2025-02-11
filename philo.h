8f27fe91ffdfd3a7ad3bfb1b4f35e4a1fee02cec
Youssef  Tabia <ytabia@e2r5p4.1337.ma>
2025-02-11 21:09:44 +0100

    parsing

---
+++ philo.h
@@ -0,0 +1,14 @@
+#ifndef PHILO_H
+#define PHILO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <limits.h>
+#include <sys/time.h>
+
+
+
+#endif
